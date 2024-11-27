9b06cbab3bdc8f7c330e5dc19629cc74856bc18b
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-27 06:51:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID|| 'sVRl2aTB#Glpe7siUwqHUsOnIjt2CXfXf_QZgTFyAD5H_OXLFOXA',
 ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/tCkmbMx/IMG-20240913-WA0041.jpg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
+AUTO_READ_CMD: process.env.AUTO_READ_CMD || "false",
 AUTO_TYPING:"true",
 AUTO_BIO:"true",
 AUTO_VOICE:"true"
