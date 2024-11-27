74e510f017ec1dd40a737884cb0cb442000198f9
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-27 06:49:21 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,5 +1,5 @@
-const { readEnv } = require('../lib/database');
-const { cmd } = require('../command');
+const config = require('../config')
+const {cmd , commands} = require('../command')
 
 // ========== ALIVE COMMAND ==========
 cmd({
